bdcfe161bf6ec6d4d02e3a867847fca81da00a24
Christina <christinaraganit@gmail.com>
2022-02-23 00:59:50 -0800

    settings modal added

---
+++ src/components/HowToPlayModal.js
@@ -0,0 +1,176 @@
+import * as React from "react";
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import { Divider } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+import "../css/HowToPlayModal.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  borderRadius: 2,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function HowToPlayModal(props) {
+  return (
+    <Modal
+      aria-labelledby="how-to-play-modal"
+      aria-describedby="how-to-play-modal"
+      open={props.howToPlayOpen}
+      onClose={props.handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.howToPlayOpen}>
+        <Box sx={style}>
+          <IconButton
+            aria-label="close"
+            onClick={props.handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <Typography variant="h6" component="h2">
+            How to play
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            Guess the <b>YORDLE</b> in six tries.
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            Each guess must be a valid five-letter word. Hit the enter button to
+            submit.
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            After each guess, the color of the tiles will change to show how
+            close your guess was to the word.
+          </Typography>
+
+          <Divider sx={{ mt: 2, mb: 2 }}></Divider>
+
+          <Typography sex={{ mb: 2 }} variant="h6" component="h2">
+            Example
+          </Typography>
+
+          <div className="modal-words-container">
+            <Box p={2} component="span" sx={{ backgroundColor: "#a5d6a7" }}>
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                B
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                R
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                A
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                N
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                D
+              </Typography>
+            </Box>
+          </div>
+          <Typography sx={{ mt: 2 }}>
+            The letter <b>B</b> is in the word and in the correct spot.
+          </Typography>
+
+          <div className="modal-words-container">
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                C
+              </Typography>
+            </Box>
+            <Box p={2} component="span" sx={{ backgroundColor: "#ffe082" }}>
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                O
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                R
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                K
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                I
+              </Typography>
+            </Box>
+          </div>
+          <Typography sx={{ mt: 2 }}>
+            The letter <b>O</b> is in the word but in the wrong spot.
+          </Typography>
+
+          <div className="modal-words-container">
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                G
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                A
+              </Typography>
+            </Box>
+            <Box p={2} component="span" sx={{ backgroundColor: "#eeeeee" }}>
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                L
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                I
+              </Typography>
+            </Box>
+            <Box p={2} component="span">
+              <Typography sx={{ mt: -0.5 }} variant="h6">
+                O
+              </Typography>
+            </Box>
+          </div>
+          <Typography sx={{ mt: 2 }}>
+            The letter <b>L</b> is not in the word in any spot.
+          </Typography>
+
+          <Divider sx={{ mt: 2, mb: 2 }}></Divider>
+
+          <Typography sx={{ mt: 2 }}>
+            A new <b>YORDLE</b> will be available each day!
+          </Typography>
+        </Box>
+      </Fade>
+    </Modal>
+  );
+}
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,37 +1,41 @@
-import * as React from "react";
 import { AppBar } from "@mui/material";
 import { Box } from "@mui/material";
 import { Toolbar } from "@mui/material";
 import { Typography } from "@mui/material";
 import { IconButton } from "@mui/material";
-import Backdrop from "@mui/material/Backdrop";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
-import { Divider } from "@mui/material";
 import HelpIcon from "@mui/icons-material/Help";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
+import React, { useState } from "react";
 import LeaderboardIcon from "@mui/icons-material/Leaderboard";
 import SettingsIcon from "@mui/icons-material/Settings";
 
 import "../css/Toolbar.css";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  borderRadius: 2,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
+import StatisticsModal from "./StatisticsModal";
+import SettingsModal from "./SettingsModal";
+import HowToPlayModal from "./HowToPlayModal";
 
 export default function ButtonAppBar() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [howToPlayOpen, setHowToPlayOpen] = useState(false);
+  const [statisticsOpen, setStatisticsOpen] = useState(false);
+
+  const handleSettingsOpen = () => {
+    setSettingsOpen(true);
+  };
+
+  const handleHowToPlayOpen = () => {
+    setHowToPlayOpen(true);
+  };
+
+  const handleStatisticsOpen = () => {
+    setStatisticsOpen(true);
+  };
+
+  const handleClose = () => {
+    setSettingsOpen(false);
+    setHowToPlayOpen(false);
+    setStatisticsOpen(false);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -42,196 +46,59 @@ export default function ButtonAppBar() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 1 }}
           >
             <MenuIcon></MenuIcon>
           </IconButton>
 
           <IconButton
-            onClick={handleOpen}
+            onClick={handleHowToPlayOpen}
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
+            sx={{ ml: 1 }}
           >
             <HelpIcon></HelpIcon>
           </IconButton>
 
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <Box sx={style}>
-                <IconButton
-                  aria-label="close"
-                  onClick={handleClose}
-                  sx={{
-                    position: "absolute",
-                    right: 8,
-                    top: 8,
-                    color: (theme) => theme.palette.grey[500],
-                  }}
-                >
-                  <CloseIcon />
-                </IconButton>
+          <HowToPlayModal
+            howToPlayOpen={howToPlayOpen}
+            handleClose={() => handleClose()}
+          ></HowToPlayModal>
 
-                <Typography variant="h6" component="h2">
-                  How to play
-                </Typography>
-                <Typography sx={{ mt: 2 }}>
-                  Guess the <b>YORDLE</b> in six tries.
-                </Typography>
-                <Typography sx={{ mt: 2 }}>
-                  Each guess must be a valid five-letter word. Hit the enter
-                  button to submit.
-                </Typography>
-                <Typography sx={{ mt: 2 }}>
-                  After each guess, the color of the tiles will change to show
-                  how close your guess was to the word.
-                </Typography>
-
-                <Divider sx={{ mt: 2, mb: 2 }}></Divider>
-
-                <Typography sex={{ mb: 2 }} variant="h6" component="h2">
-                  Example
-                </Typography>
-
-                <div class="modal-words-container">
-                  <Box
-                    p={2}
-                    component="span"
-                    sx={{ backgroundColor: "#6fbf73" }}
-                  >
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      B
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      R
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      A
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      N
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      D
-                    </Typography>
-                  </Box>
-                </div>
-                <Typography sx={{ mt: 2 }}>
-                  The letter <b>B</b> is in the word and in the correct spot.
-                </Typography>
-
-                <div class="modal-words-container">
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      C
-                    </Typography>
-                  </Box>
-                  <Box
-                    p={2}
-                    component="span"
-                    sx={{ backgroundColor: "#ffd54f" }}
-                  >
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      O
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      R
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      K
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      I
-                    </Typography>
-                  </Box>
-                </div>
-                <Typography sx={{ mt: 2 }}>
-                  The letter <b>O</b> is in the word but in the wrong spot.
-                </Typography>
-
-                <div class="modal-words-container">
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      G
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      A
-                    </Typography>
-                  </Box>
-                  <Box
-                    p={2}
-                    component="span"
-                    sx={{ backgroundColor: "#bdbdbd" }}
-                  >
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      L
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      I
-                    </Typography>
-                  </Box>
-                  <Box p={2} component="span">
-                    <Typography sx={{ mt: -0.5 }} variant="h6">
-                      O
-                    </Typography>
-                  </Box>
-                </div>
-                <Typography sx={{ mt: 2 }}>
-                  The letter <b>L</b> is not in the word in any spot.
-                </Typography>
-
-                <Divider sx={{ mt: 2, mb: 2 }}></Divider>
-
-                <Typography sx={{ mt: 2 }}>
-                  A new <b>YORDLE</b> will be available each day!
-                </Typography>
-              </Box>
-            </Fade>
-          </Modal>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Yordle
           </Typography>
           <IconButton
+            onClick={handleStatisticsOpen}
             size="large"
             edge="end"
             color="inherit"
-            sx={{ mr: 0.25 }}
+            sx={{ mr: 1 }}
             aria-label="menu"
           >
             <LeaderboardIcon></LeaderboardIcon>
           </IconButton>
-          <IconButton size="large" edge="end" color="inherit" aria-label="menu">
+
+          <StatisticsModal
+            statisticsOpen={statisticsOpen}
+            handleClose={() => handleClose()}
+          ></StatisticsModal>
+
+          <IconButton
+            onClick={handleSettingsOpen}
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+          >
             <SettingsIcon></SettingsIcon>
           </IconButton>
+
+          <SettingsModal
+            settingsOpen={settingsOpen}
+            handleClose={() => handleClose()}
+          ></SettingsModal>
         </Toolbar>
       </AppBar>
     </Box>
